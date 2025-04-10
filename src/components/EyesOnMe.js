033b275951fe7cbb6a498c2373553463b7bc7484
Brian Tallam <tallambrian633@gmail.com>
2025-04-10 13:05:33 +0300

    handling

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,11 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+    return (
+        <div className="eyes-on-me">
+            <button onFocus={() => console.log('Good!')}onBlur={() => console.log('Hey! Eyes on me!')} > Eyes on me</button>
+        </div>
+    );
+}
+export default EyesOnMe;
